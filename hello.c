b0f27327b7cffb115b69d77c3adbef461ff43dac
rbalsxzcv <rbalsxzcv2004@naver.com>
2026-09-15 19:57:39 +0900

    asdfasdf

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello world!!");
+    for(int i = 0;i<15;i++){
+        for(int j = 0;j<i*2+1;j++)
+            printf("*");
+        printf("\n");    
+    }
     return 0;
 }
